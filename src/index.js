6182d9e87769790d3f796d9b768a595e9617d853
linsanity-dev <3690553287@qq.com>
2024-08-24 13:56:41 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.lin568.eu.org": dockerHub,
+  "quay.lin568.eu.org": "https://quay.io",
+  "gcr.lin568.eu.org": "https://gcr.io",
+  "k8s-gcr.lin568.eu.org": "https://k8s.gcr.io",
+  "k8s.lin568.eu.org": "https://registry.k8s.io",
+  "ghcr.lin568.eu.org": "https://ghcr.io",
+  "cloudsmith.lin568.eu.org": "https://docker.cloudsmith.io",
+  "ecr.lin568.eu.org": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
